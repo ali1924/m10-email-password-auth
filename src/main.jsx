c18b36023465e7998e9ab8c2592db3f4d0dc0c79
ali1924 <mohammadali229987@gmail.com>
2023-04-13 23:47:15 +0600

    react bootstrap form

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Main from './components/Layout/Main'
 import Home from './components/Home/Home'
 import Login from './components/Login/Login'
 import Register from './components/Register/Register'
+import RegisterRBS from './components/RegisterRBS/RegisterRBS'
 const router = createBrowserRouter([
   {
     path: '/',
@@ -23,6 +25,10 @@ const router = createBrowserRouter([
       {
         path: '/register',
         element:<Register></Register>
+      },
+      {
+        path: '/register-rbs',
+        element:<RegisterRBS></RegisterRBS>
       }
     ]
   }
